a6cda6b1547e65f5e2f8bd921ef4a94a11bd2684
Sahil Ingale <60597512+sahilingale629@users.noreply.github.com>
2023-11-11 15:09:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+      message: "enter the url: ",
+
+      name: "URL",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr-img.png"));
+
+    fs.writeFile("url.txt", url, (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+    } else {
+    }
+  });
